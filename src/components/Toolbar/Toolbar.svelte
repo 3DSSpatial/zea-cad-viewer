0ca4769d4658ed16d751d240c91eff894e407617
Philip Taylor <phil@zea.live>
2021-07-14 10:20:36 -0400

    refactor: code for changing render modes is moved to helper file.

--- src/components/Toolbar/Toolbar.svelte
+++ src/components/Toolbar/Toolbar.svelte
@@ -19,6 +19,7 @@
   import IconRenderModeHiddenLine from '../icons/IconRenderModeHiddenLine.svelte'
 
   import { APP_DATA } from '../../stores/appData'
+  import { RENDER_MODES, changeRenderMode } from '../../helpers/renderModes'
 
   const {
     Vec3,
@@ -141,208 +142,26 @@
 
   // ////////////////////////////////////////
   // Render Modes
-
-  const RENDER_MODES = {
-    WIREFRAME: Symbol(),
-    FLAT: Symbol(),
-    HIDDEN_LINE: Symbol(),
-    SHADED: Symbol(),
-    SHADED_AND_EDGES: Symbol(),
-    PBR: Symbol(),
-  }
-
   // The default materials are standard shiny surfaces.(PBR)
   let mode = RENDER_MODES.PBR
-  // Materials are shared among geom items.
-  // We keep track of the material the geom item was using after load.
-  // and for each unique material, make a clone and modify it for
-  // each render mode.
-  const materials = {}
-
-  // The mapping that remembers what material was assigned to the
-  // geomItem at load time.
-  const geomItemToMaterialMapping = {}
-  const createAndAssignMaterial = (geomItem, mode, cb) => {
-    const geomItemId = geomItem.getId()
-    if (mode != RENDER_MODES.PBR) {
-      const material = geomItem.getParameter('Material').getValue()
-      const materialId = material.getId()
-      if (!geomItemToMaterialMapping[geomItemId]) {
-        geomItemToMaterialMapping[geomItemId] = materialId
-      }
-      if (!materials[materialId]) {
-        materials[materialId] = {}
-        materials[materialId][RENDER_MODES.PBR] = material
-      }
-    }
-    const materialId = geomItemToMaterialMapping[geomItemId]
-    if (!materials[materialId][mode]) {
-      // Clone the PBR material and use it as a basis for the new material.
-      const newMaterial = materials[materialId][RENDER_MODES.PBR].clone()
-      if (cb) cb(newMaterial)
-      materials[materialId][mode] = newMaterial
-    }
-    const result = materials[materialId][mode]
-    geomItem.getParameter('Material').setValue(result)
-  }
   const handleChangeRenderModeWireframe = () => {
-    if (mode == RENDER_MODES.WIREFRAME) {
-      return
-    }
+    changeRenderMode(RENDER_MODES.WIREFRAME)
     mode = RENDER_MODES.WIREFRAME
-
-    const { assets } = $APP_DATA
-    assets.traverse((item) => {
-      if (item instanceof GeomItem) {
-        const geom = item.getParameter('Geometry').getValue()
-        if (geom instanceof Mesh || geom instanceof MeshProxy) {
-          item.getParameter('Visible').setValue(false)
-        }
-        createAndAssignMaterial(item, RENDER_MODES.WIREFRAME, (newMaterial) => {
-          newMaterial.setName('Wireframe')
-        })
-      }
-    })
   }
   const handleChangeRenderModeFlat = () => {
-    if (mode == RENDER_MODES.FLAT) {
-      return
-    }
-    mode = RENDER_MODES.FLAT
-
-    const { assets, scene, renderer } = $APP_DATA
-
-    renderer.outlineThickness = 1
-    renderer.outlineColor = new Color(0.2, 0.2, 0.2, 1)
-
-    // const backgroundColor = scene
-    // .getSettings()
-    // .getParameter('BackgroundColor')
-    // .getValue()
-    assets.traverse((item) => {
-      if (item instanceof GeomItem) {
-        const geom = item.getParameter('Geometry').getValue()
-        if (geom instanceof Mesh || geom instanceof MeshProxy) {
-          item.getParameter('Visible').setValue(true)
-        }
-        createAndAssignMaterial(item, RENDER_MODES.FLAT, (newMaterial) => {
-          newMaterial.setName('Flat')
-          const shaderName = newMaterial.getShaderName()
-          if (shaderName == 'LinesShader') {
-            if (newMaterial.hasParameter('OccludedStippleValue')) {
-              newMaterial.getParameter('OccludedStippleValue').setValue(1)
-            }
-          } else {
-            newMaterial.setShaderName('FlatSurfaceShader')
-
-            // Note: Assigning the background color makes the surfaces
-            // blend in with the background. Another option would be to
-            // desaturate the current color.
-            // newMaterial.getParameter('BaseColor').setValue(backgroundColor)
-          }
-        })
-      }
-    })
+    changeRenderMode(RENDER_MODES.FLAT)
     mode = RENDER_MODES.FLAT
   }
   const handleChangeRenderModeHiddenLine = () => {
-    if (mode == RENDER_MODES.HIDDEN_LINE) {
-      return
-    }
-    mode = RENDER_MODES.HIDDEN_LINE
-
-    const { assets, renderer } = $APP_DATA
-
-    renderer.outlineThickness = 1
-    renderer.outlineColor = new Color(0.2, 0.2, 0.2, 1)
-
-    assets.traverse((item) => {
-      if (item instanceof GeomItem) {
-        const geom = item.getParameter('Geometry').getValue()
-        if (geom instanceof Mesh || geom instanceof MeshProxy) {
-          item.getParameter('Visible').setValue(true)
-        }
-        createAndAssignMaterial(
-          item,
-          RENDER_MODES.HIDDEN_LINE,
-          (newMaterial) => {
-            newMaterial.setName('HiddenLine')
-            const shaderName = newMaterial.getShaderName()
-            if (shaderName == 'LinesShader') {
-              if (newMaterial.hasParameter('OccludedStippleValue')) {
-                newMaterial.getParameter('StippleScale').setValue(0.02)
-                newMaterial.getParameter('StippleValue').setValue(0)
-                newMaterial.getParameter('OccludedStippleValue').setValue(0.6)
-              }
-            } else {
-              newMaterial.setShaderName('FlatSurfaceShader')
-              // newMaterial.getParameter('BaseColor').setValue(backgroundColor)
-            }
-          }
-        )
-      }
-    })
+    changeRenderMode(RENDER_MODES.HIDDEN_LINE)
     mode = RENDER_MODES.HIDDEN_LINE
   }
   const handleChangeRenderModeShadedAndEdges = () => {
-    if (mode == RENDER_MODES.SHADED_AND_EDGES) {
-      return
-    }
-
-    const { assets, renderer } = $APP_DATA
-
-    renderer.outlineThickness = 1
-    renderer.outlineColor = new Color(0.2, 0.2, 0.2, 1)
-
-    assets.traverse((item) => {
-      if (item instanceof GeomItem) {
-        const geom = item.getParameter('Geometry').getValue()
-        if (geom instanceof Mesh || geom instanceof MeshProxy) {
-          item.getParameter('Visible').setValue(true)
-        }
-        if (geom instanceof Lines || geom instanceof LinesProxy) {
-          item.getParameter('Visible').setValue(true)
-        }
-        createAndAssignMaterial(
-          item,
-          RENDER_MODES.SHADED_AND_EDGES,
-          (newMaterial) => {
-            newMaterial.setName('ShadedAndEdges')
-            const shaderName = newMaterial.getShaderName()
-            if (shaderName == 'LinesShader') {
-              if (newMaterial.hasParameter('OccludedStippleValue')) {
-                newMaterial.getParameter('OccludedStippleValue').setValue(1)
-              }
-            } else {
-              newMaterial.setShaderName('SimpleSurfaceShader')
-            }
-          }
-        )
-      }
-    })
+    changeRenderMode(RENDER_MODES.SHADED_AND_EDGES)
     mode = RENDER_MODES.SHADED_AND_EDGES
   }
   const handleChangeRenderModePBR = () => {
-    if (mode == RENDER_MODES.PBR) {
-      return
-    }
-    const { assets, renderer } = $APP_DATA
-
-    renderer.outlineThickness = 1
-    renderer.outlineColor = new Color(0.2, 0.2, 0.2, 1)
-
-    assets.traverse((item) => {
-      if (item instanceof GeomItem) {
-        const geom = item.getParameter('Geometry').getValue()
-        if (geom instanceof Mesh || geom instanceof MeshProxy) {
-          item.getParameter('Visible').setValue(true)
-        }
-        if (geom instanceof Lines || geom instanceof LinesProxy) {
-          item.getParameter('Visible').setValue(true)
-        }
-        createAndAssignMaterial(item, RENDER_MODES.PBR)
-      }
-    })
+    changeRenderMode(RENDER_MODES.PBR)
     mode = RENDER_MODES.PBR
   }
 </script>
