59f96406c6e4b6d5c4c45bfbeef0db11c12ea928
Philip Taylor <phil@zea.live>
2021-08-12 17:07:08 -0400

    feat: Integrated measurement tools into Toolbar.

---
+++ src/components/icons/IconMeasureDistance.svelte
@@ -0,0 +1,71 @@
+<script>
+</script>
+
+<svg
+  version="1.1"
+  viewBox="0.0 0.0 108.9238845144357 108.94750656167979"
+  fill="none"
+  stroke="none"
+  stroke-linecap="square"
+  stroke-miterlimit="10"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  xmlns="http://www.w3.org/2000/svg"
+  ><clipPath id="p.0"
+    ><path
+      d="m0 0l108.92388 0l0 108.94751l-108.92388 0l0 -108.94751z"
+      clip-rule="nonzero"
+    /></clipPath
+  ><g clip-path="url(#p.0)"
+    ><path
+      fill="#000000"
+      fill-opacity="0.0"
+      d="m0 0l108.92388 0l0 108.94751l-108.92388 0z"
+      fill-rule="evenodd"
+    /><path
+      fill="#f9ce03"
+      d="m13.020997 56.51719l0 0c0 -3.5631065 2.8884678 -6.4515724 6.4515734 -6.4515724l75.978745 0c1.7110596 0 3.3520432 0.679718 4.5619507 1.8896217c1.2098999 1.2099037 1.8896179 2.8508873 1.8896179 4.5619507l0 25.80552c0 3.5631027 -2.888466 6.4515686 -6.4515686 6.4515686l-75.978745 0l0 0c-3.5631056 0 -6.4515734 -2.888466 -6.4515734 -6.4515686z"
+      fill-rule="evenodd"
+    /><path
+      stroke="#000000"
+      stroke-width="3.0"
+      stroke-linejoin="round"
+      stroke-linecap="butt"
+      d="m13.020997 56.51719l0 0c0 -3.5631065 2.8884678 -6.4515724 6.4515734 -6.4515724l75.978745 0c1.7110596 0 3.3520432 0.679718 4.5619507 1.8896217c1.2098999 1.2099037 1.8896179 2.8508873 1.8896179 4.5619507l0 25.80552c0 3.5631027 -2.888466 6.4515686 -6.4515686 6.4515686l-75.978745 0l0 0c-3.5631056 0 -6.4515734 -2.888466 -6.4515734 -6.4515686z"
+      fill-rule="evenodd"
+    /><path
+      fill="#000000"
+      fill-opacity="0.0"
+      d="m31.444708 33.401573l57.16535 0"
+      fill-rule="evenodd"
+    /><path
+      stroke="#000000"
+      stroke-width="3.0"
+      stroke-linejoin="round"
+      stroke-linecap="butt"
+      d="m31.444708 33.401573l57.16535 0"
+      fill-rule="evenodd"
+    /><path
+      fill="#f9ce03"
+      d="m82.81548 27.070866l19.086617 6.3307076l-19.086617 6.3307114z"
+      fill-rule="evenodd"
+    /><path
+      stroke="#000000"
+      stroke-width="3.0"
+      stroke-linejoin="round"
+      stroke-linecap="butt"
+      d="m82.81548 27.070866l19.086617 6.3307076l-19.086617 6.3307114z"
+      fill-rule="evenodd"
+    /><path
+      fill="#f9ce03"
+      d="m31.444708 39.732285l-19.086615 -6.3307114l19.086615 -6.3307076z"
+      fill-rule="evenodd"
+    /><path
+      stroke="#000000"
+      stroke-width="3.0"
+      stroke-linejoin="round"
+      stroke-linecap="butt"
+      d="m31.444708 39.732285l-19.086615 -6.3307114l19.086615 -6.3307076z"
+      fill-rule="evenodd"
+    /></g
+  ></svg
+>
