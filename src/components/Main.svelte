e583a005ba7b5f9f1409137469bc328c00367971
Philip Taylor <phil@zea.live>
2021-09-09 08:45:51 -0400

    chore: Removed Zea Cloud Client login as we no longer support that approach to loading data from zea-cloud

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -19,7 +19,6 @@
   import { ui } from '../stores/ui.js'
   import { selectionManager } from '../stores/selectionManager.js'
   import { scene } from '../stores/scene.js'
-  import { zeaCloudClient } from '../stores/zeaCloudClient'
 
   import { createClient } from '../ChannelMessenger.js'
   import buildTree from '../helpers/buildTree'
@@ -94,42 +93,6 @@
     return loadZCADAsset(url)
   }
   /** LOAD ASSETS METHODS END */
-
-  /* {{{ Zea Cloud Client. */
-  if (!embeddedMode) {
-    zeaCloudClient.subscribe(async (client) => {
-      // We need this check since `undefined` is
-      // the initial default value for derived stores.
-      // See:
-      // https://svelte.dev/docs#derived
-      if (!client) {
-        return
-      }
-
-      const organizationId = urlParams.get('organization-id')
-      const projectId = urlParams.get('project-id')
-      const fileId = urlParams.get('file-id')
-
-      if (!organizationId || !projectId || !fileId) {
-        throw new Error(
-          'The Zea Cloud Client requires all of the `organization-id`, `project-id`, and `file-id` params.'
-        )
-      }
-
-      const organization = await client.findOrganization(organizationId)
-      const project = await organization.findProject(projectId)
-      await project.fetchLatestVersion()
-      const file = project.getFileById(fileId)
-      const processName = 'cad'
-      const filename = 'output.zcad'
-      const assetUrl = await file.getSignedUrlToReadDownstream(
-        processName,
-        filename
-      )
-      loadAsset(assetUrl, filename)
-    })
-  }
-  /* }}} Zea Cloud Client. */
 
   onMount(async () => {
     renderer = new GLRenderer(canvas)
@@ -303,11 +266,9 @@
     /** FPS DISPLAY END */
 
     /** LOAD ASSETS START */
-    if (embeddedMode) {
-      if (urlParams.has('zcad')) {
-        const assetUrl = urlParams.get('zcad')
-        loadAsset(assetUrl, assetUrl)
-      }
+    if (urlParams.has('zcad')) {
+      const assetUrl = urlParams.get('zcad')
+      loadAsset(assetUrl, assetUrl)
     }
     /** LOAD ASSETS END */
 
--- src/layouts/LayoutProtected.svelte
+++ src/layouts/LayoutProtected.svelte
@@ -1,39 +1,44 @@
 <script>
   import { redirect } from '@roxi/routify'
 
-  import SplashScreen from '../components/SplashScreen.svelte'
-  import { authClient, currentUser } from '../stores/auth'
+  // import SplashScreen from '../components/SplashScreen.svelte'
+  // import { authClient, currentUser } from '../stores/auth'
   const urlParams = new URLSearchParams(window.location.search)
 
-  const embeddedMode = urlParams.has('embedded')
+  // Note: PT 09-09-2021: We are disabling all authentication in the zea-cad-viewer
+  // as we are not supporting collaboration, as this app is intended to be integrated
+  // into a client application. I believe a better solution would be to remove the 'LayoutProtected'
+  // component altogether.
+  // const embeddedMode = urlParams.has('embedded')
 
-  if (embeddedMode) {
-    $redirect(`/?${urlParams.toString()}`)
-  } else {
-    authClient.subscribe(async (client) => {
-      // We need this check since `null` is
-      // the initial value for the `auth` store.
-      if (!client) {
-        return
-      }
+  // if (embeddedMode) {
+  $redirect(`/?${urlParams.toString()}`)
+  // } else {
+  //   authClient.subscribe(async (client) => {
+  //     // We need this check since `null` is
+  //     // the initial value for the `auth` store.
+  //     if (!client) {
+  //       return
+  //     }
 
-      const isAuthenticated = await client.isAuthenticated()
+  //     const isAuthenticated = await client.isAuthenticated()
+  //     console.log('isAuthenticated', isAuthenticated)
 
-      if (!isAuthenticated) {
-        client.loginWithRedirect({
-          redirect_uri: `${window.location.origin}/sign-in-callback${window.location.search}`,
-        })
-      }
-    })
-  }
+  //     if (!isAuthenticated) {
+  //       client.loginWithRedirect({
+  //         redirect_uri: `${window.location.origin}/sign-in-callback${window.location.search}`,
+  //       })
+  //     }
+  //   })
+  // }
 </script>
 
-{#if embeddedMode}
-  <slot />
-{:else if $authClient}
+<!-- {#if embeddedMode} -->
+<slot />
+<!-- {:else if $authClient}
   {#if $currentUser}
     <slot />
   {/if}
 {:else}
   <SplashScreen />
-{/if}
+{/if} -->
