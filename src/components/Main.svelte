75c6df86cff0d32b4254ebbe91f50981b4b34e2b
Philip Taylor <phil@zea.live>
2022-03-07 11:48:55 -0500

    chore: disable broken view-cube.

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -191,9 +191,9 @@
 
     /** UX START */
 
-    const viewCube = document.getElementById('view-cube')
-    console.log('Setup Viewcube:', viewCube, renderer.getViewport())
-    viewCube.setViewport(renderer.getViewport())
+    // const viewCube = document.getElementById('view-cube')
+    // console.log('Setup Viewcube:', viewCube, renderer.getViewport())
+    // viewCube.setViewport(renderer.getViewport())
 
     //long touch support
     var longTouchTimer = 0
@@ -531,7 +531,7 @@
 <main class="Main flex-1 relative">
   <canvas bind:this={canvas} class="absolute h-full w-full" />
 
-  <zea-view-cube id="view-cube" />
+  <!-- <zea-view-cube id="view-cube" /> -->
 
   {#if !fileLoaded}
     <DropZone bind:files on:changeFile={handleCadFile} {fileLoaded} />
