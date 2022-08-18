dcba3d03ec66b937dd0223ceb0c77138cb6b569a
PhilippeChevreau <pcu@3ds.com>
2022-08-18 15:01:07 -0600

    adding runtime
    ./Dist added

---
+++ libs/zea-engine/plugins.umd.js
@@ -0,0 +1,1 @@
+!function(e){"function"==typeof define&&define.amd?define(e):e()}((function(){"use strict";console.warn("The plugins.umd.js script tag is now deprecated. Please remove it to avoid this warning.")}));
